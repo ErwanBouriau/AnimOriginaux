76af375fee058311f57b14db660e4cfed09fc98a
unknown <erwan.bouriau@etu.univ-grenoble-alpes.fr>
2020-04-02 14:56:54 +0200

    design form et animal

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnimalController extends AbstractController
@@ -24,13 +25,15 @@ class AnimalController extends AbstractController
     /**
      * @Route("/animaux/{id}", name="animalInfo")
      */
-    public function pageInfo($id, AnimalRepository $animalRepository)
+    public function pageInfo($id, AnimalRepository $animalRepository, Request $request)
     {
         $animal = $animalRepository->getAnimalById($id);
+        $request2 = $request->get('location');
 
         return $this->render('animal/animal.html.twig', [
             'controller_name' => 'AnimalController',
-            'animal' => $animal[0]
+            'animal' => $animal[0],
+            'requete' => $request2
         ]);
     }
 }
